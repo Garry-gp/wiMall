2d2f953b263579f2cb14a1e26dc5eab3f1b9ad5e
Garry-gp <15986634667@163.com>
2018-03-20 20:16:22 +0800

    优化用户记录管理列表

--- demo/src/main/resources/static/defined/defined_layout.js
+++ demo/src/main/resources/static/defined/defined_layout.js
@@ -3,7 +3,7 @@
         if ($('#content').tabs('exists', title)){
             $('#content').tabs('select', title);
         } else {
-            var content = '<iframe scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
+            var content = '<iframe scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:98%;"></iframe>';
             $('#content').tabs('add',{
                 title:title,
                 content:content,
